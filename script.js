f882bea23ad8eb94c52e28aad6fe53adfbb2a690
JiriVanekk <134601436+JiriVanekk@users.noreply.github.com>
2023-05-25 13:30:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,30 @@
-// Define the URLs you want to display
-var urls = [
-  'https://www.example.com/page1',
-  'https://www.example.com/page2',
-  'https://www.example.com/page3'
-];
-
-// Get the content element
-var contentElement = document.getElementById('content');
-
-// Set the initial index
-var currentIndex = 0;
-
-// Function to fetch and display content
-function fetchAndDisplayContent() {
-  // Fetch the content from the current URL
-  fetch(urls[currentIndex])
-    .then(function(response) {
-      return response.text();
-    })
-    .then(function(data) {
-      // Display the content
-      contentElement.innerHTML = data;
-    })
-    .catch(function(error) {
-      console.log('Error fetching content:', error);
-    });
+/**
+ * This function displays the contents of a URL that changes every 30 seconds.
+ * The list of URLs is stored in an array.
+ */
+function displayURLContents() {
+  const urls = [
+   
+    "https://status.rondainvest.cz/",
+    "https://online.rondainvest.cz/login"
+  ];
+  
+  let index = 0;
+  
+  setInterval(() => {
+    // Make a GET request to the current URL
+    fetch(urls[index])
+      .then(response => response.text())
+      .then(data => {
+        // Display the contents of the URL
+        console.log(data);
+      })
+      .catch(error => {
+        // Log any errors that occur
+        console.error(error);
+      });
+    
+    // Increment the index or reset it to 0 if it exceeds the length of the array
+    index = (index + 1) % urls.length;
+  }, 30000);
 }
-
-// Function to swap between URLs
-function swapContent() {
-  // Increment the index
-  currentIndex++;
-
-  // Reset the index if it goes beyond the number of URLs
-  if (currentIndex >= urls.length) {
-    currentIndex = 0;
-  }
-
-  // Fetch and display the content
-  fetchAndDisplayContent();
-}
-
-// Fetch and display the initial content
-fetchAndDisplayContent();
-
-// Swap the content every 5 seconds (adjust the interval as needed)
-setInterval(swapContent, 5000);
