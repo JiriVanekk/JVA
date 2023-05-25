e43a0cfb91dc9b2938380c1a22877face0f73eb3
JiriVanekk <134601436+JiriVanekk@users.noreply.github.com>
2023-05-25 13:08:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
+// Define the URLs you want to display
 var urls = [
-    "https://status.rondainvest.cz/",
-    "https://rondainvest.cz/"
+  'https://www.example.com/page1',
+  'https://www.example.com/page2',
+  'https://www.example.com/page3'
 ];
 
-var currentIndex = 0;
-var urlDisplay = document.getElementById("url-display");
+// Get the content element
+var contentElement = document.getElementById('content');
 
-function changeURL() {
-    urlDisplay.innerHTML = urls[currentIndex];
-    currentIndex = (currentIndex + 1) % urls.length;
+// Set the initial index
+var currentIndex = 0;
+
+// Function to fetch and display content
+function fetchAndDisplayContent() {
+  // Fetch the content from the current URL
+  fetch(urls[currentIndex])
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(data) {
+      // Display the content
+      contentElement.innerHTML = data;
+    })
+    .catch(function(error) {
+      console.log('Error fetching content:', error);
+    });
 }
 
-// Change URL every 5 seconds
-setInterval(changeURL, 5000);
+// Function to swap between URLs
+function swapContent() {
+  // Increment the index
+  currentIndex++;
+
+  // Reset the index if it goes beyond the number of URLs
+  if (currentIndex >= urls.length) {
+    currentIndex = 0;
+  }
+
+  // Fetch and display the content
+  fetchAndDisplayContent();
+}
+
+// Fetch and display the initial content
+fetchAndDisplayContent();
+
+// Swap the content every 5 seconds (adjust the interval as needed)
+setInterval(swapContent, 5000);
