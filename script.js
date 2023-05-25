e83e1fbc0a66be580c21ab56b5b1dfe5815abfa1
JiriVanekk <134601436+JiriVanekk@users.noreply.github.com>
2023-05-25 14:03:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-/**
- * This function displays the contents of a URL that changes every 30 seconds.
- * The list of URLs is stored in an array.
- */
 function displayURLContents() {
   const urls = [
-   
     "https://status.rondainvest.cz/",
     "https://online.rondainvest.cz/login"
   ];
-  
+
   let index = 0;
-  
+  const contentElement = document.getElementById("content");
+
   setInterval(() => {
-    // Make a GET request to the current URL
     fetch(urls[index])
       .then(response => response.text())
       .then(data => {
-        // Display the contents of the URL
-        console.log(data);
+        // Display the contents of the URL inside the content element
+        contentElement.innerHTML = data;
       })
       .catch(error => {
         // Log any errors that occur
         console.error(error);
       });
-    
-    // Increment the index or reset it to 0 if it exceeds the length of the array
+
     index = (index + 1) % urls.length;
   }, 30000);
 }
+
+// Call the displayURLContents function to start displaying the URL contents
+displayURLContents();
